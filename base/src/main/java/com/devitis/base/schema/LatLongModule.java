4ffb68243996e977e0f246e6db10a82647fa082c
Diana <diana.sinenchenko@gmail.com>
2019-03-20 12:40:20 +0200

    create realm module for LatLong class

---
+++ base/src/main/java/com/devitis/base/schema/LatLongModule.java
@@ -0,0 +1,10 @@
+package com.devitis.base.schema;
+
+import io.realm.annotations.RealmModule;
+
+/**
+ * Created by Diana on 20.03.2019.
+ */
+@RealmModule (classes = {LatLong.class})
+public class LatLongModule {
+}
